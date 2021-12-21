c62c46170a0aa9aaf00e41dc4a94bc4b2d096d67
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-12-21 20:18:09 +0000

    feat: add coment model

--- leannes_learners_data/admin.py
+++ leannes_learners_data/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin 
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -13,3 +13,13 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_at')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_at', 'approved')
+    list_filter = ('approved', 'created_at')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
