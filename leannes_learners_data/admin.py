1c2e9b87b0dbba2f81eb9adaa7c7a613195ead98
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-12-21 20:12:15 +0000

    feat: add post model

--- leannes_learners_data/admin.py
+++ leannes_learners_data/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+admin.site.register(Post)
+
+# Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_at')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
--- leannes_learners_data/models.py
+++ leannes_learners_data/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+# code sampled from a previous walkthrough - Code Institues " I blog therefore I am"
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
