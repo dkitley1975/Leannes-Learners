a7ddfcdc515cee425162eadd071c3dcbe9d6281d
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-12-30 12:19:31 +0000

    feat: added basic authourisation with allauth

--- leannes_learners_data/urls.py
+++ leannes_learners_data/urls.py
@@ -1,7 +1,8 @@
+from django.urls import path, include
 from . import views
-from django.urls import path
 
 urlpatterns = [
     path('', views.BlogList.as_view(), name='home'),
     # path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_detail'),
-	]
+    path('accounts/', include('allauth.urls')),
+    ]
--- leannes_learners_data/views.py
+++ leannes_learners_data/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Blog
-
 # Create your views here.
 
 
