3714a64bd24f175d7ecb2479dd783626653d7aed
David Kitley-McNamara <david@kitley-mcnamara.com>
2022-01-07 10:35:09 +0000

    fix: Update to the navbar and footer
    added a Navbar dropfown facility

--- leannes_learners_data/urls.py
+++ leannes_learners_data/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.TestimonialList.as_view(), name='home'),
-    path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_post_view'),
     path('blog', views.BlogPage.as_view(), name='blogs'),
-    path('like/<slug:slug>', views.LikePost.as_view(), name='like'),
     path('accounts/', include('allauth.urls')),
+    path('like/<slug:slug>', views.LikePost.as_view(), name='like'),
+    path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_post_view'),
+    
+    
+    # path('URL', views.VIEWS-FORM-CLASS-NAME.as_view(), name='PAGE-NAME'),
     ]
