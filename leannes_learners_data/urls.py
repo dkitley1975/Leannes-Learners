3654b9c3fc7283f85e261bc332d476f8a613dfd5
David Kitley-McNamara <david@kitley-mcnamara.com>
2022-01-09 08:04:32 +0000

    feat: added About Us Page, styling still required

--- leannes_learners_data/urls.py
+++ leannes_learners_data/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.Testimonials.as_view(), name='home'),
+    path('about_us', views.AboutUsPage.as_view(), name='about_us'),
     path('blog', views.BlogPage.as_view(), name='blogs'),
-    path('pass_plus', views.Passplus.as_view(), name='pass_plus'),
+    path('pass_plus', views.PassPlusPage.as_view(), name='pass_plus'),
     path('accounts/', include('allauth.urls')),
     path('like/<slug:slug>', views.LikePost.as_view(), name='like'),
     path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_post_view'),
