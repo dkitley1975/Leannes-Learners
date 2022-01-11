e725078b57206fd295eda24b9982fc694808a1a9
David Kitley-McNamara <david@kitley-mcnamara.com>
2022-01-11 12:49:42 +0000

    feat: added favicons

--- leannes_learners_data/urls.py
+++ leannes_learners_data/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.contrib.staticfiles.storage import StaticFilesStorage, staticfiles_storage
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
@@ -11,4 +13,20 @@ urlpatterns = [
     path('like/<slug:slug>', views.LikePost.as_view(), name='like'),
     path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_post_view'),
     # path('URL', views.VIEWS-FORM-CLASS-NAME.as_view(), name='PAGE-NAME'),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
+    path(
+        "apple-touch-icon.png",
+        RedirectView.as_view(url=staticfiles_storage.url("apple-touch-icon.png")),
+    ),
+    path(
+        "maskable-icon.png",
+        RedirectView.as_view(url=staticfiles_storage.url("maskable-icon.png")),
+    ),
+    path(
+        "site.webmanifest",
+        RedirectView.as_view(url=staticfiles_storage.url("site.webmanifest")),
+    ),
     ]
