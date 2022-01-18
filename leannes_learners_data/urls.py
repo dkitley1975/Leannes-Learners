7e328f56af74f23367ac29d223e64af383201f48
David Kitley-McNamara <david@kitley-mcnamara.com>
2022-01-18 11:23:15 +0000

    fix: navigation url error - within blog view
    URL in correctly had / after causing the navigation to always look at the blog page as the root for navigation purposes.

--- leannes_learners_data/urls.py
+++ leannes_learners_data/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('pass_plus', views.PassPlusPage.as_view(), name='pass_plus'),
     path('prices', views.PricesPage.as_view(), name='prices'),
     path('like/<slug:slug>', views.LikePost.as_view(), name='like'),
-    path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_post_view'),
+    path('<slug:slug>', views.BlogDetail.as_view(), name='blog_post_view'),
 
     # path('URL', views.VIEWS-FORM-CLASS-NAME.as_view(), name='PAGE-NAME'),
     ]
