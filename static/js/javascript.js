b571c7600ee898b28145a3d3cb2222f232eed95e
David Kitley-McNamara <david@kitley-mcnamara.com>
2022-01-20 17:09:49 +0000

    feat: added flash messages, time out
    javascript functionality to the flash messages to time out after 2 secs

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,33 +1,6 @@
-// Copyright solution researched and resourced on stackoverflow
-// document.getElementById('copyright-year').appendChild(document.createTextNode(new Date().getFullYear()));
-
-// // script to auto close the alert messages
-// setTimeout(function () {
-//     let messages = document.getElementById('alert-msg');
-//     let alert = new bootstrap.Alert(messages);
-//     alert.close();
-// }, 2500);
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict';
-
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation');
-
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms).forEach(function (form) {
-        form.addEventListener(
-            'submit',
-            function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-
-                form.classList.add('was-validated');
-            },
-            false
-        );
-    });
-})();
+// script to auto close the alert messages
+setTimeout(function () {
+	if ($('#msg').length > 0) {
+		$('#msg').remove();
+	}
+}, 2000);
