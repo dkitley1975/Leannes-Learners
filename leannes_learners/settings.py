eeace909ad4c81efbd51770bd1bcb3fad35e2f67
David Kitley-McNamara <david@kitley-mcnamara.com>
2022-01-20 18:16:03 +0000

    chore: Turned off  Deployment settings

--- leannes_learners/settings.py
+++ leannes_learners/settings.py
@@ -31,15 +31,16 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.2/howto/c/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# SECURITY WARNING: don't run with debug turned on in deployment!
+DEBUG = True
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+# SECURITY WARNING: activate the below for deployment!
+# X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "leannes-learners.herokuapp.com",]
@@ -113,7 +114,8 @@ WSGI_APPLICATION = 'leannes_learners.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# TODO before Deployment remove the if else statement and replace with this    DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+# TODO before Deployment remove the if else statement and replace with this    
+# DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 if RUNLOCAL is True:
     DATABASES = {
