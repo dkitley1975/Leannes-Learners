264cd514c322e68f57c743ff16d8429c57d8a96b
David Kitley-McNamara <david@kitley-mcnamara.com>
2022-02-07 20:17:12 +0000

    feat: edit progile page added and registration form amended to include first and last names

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from users import views
 
 urlpatterns = [
-    path('register/', views.NewUserRegistrationView.as_view(), name='register'),
+    path('register/', views.UserRegistrationView.as_view(), name='register'),
+    path('edit_profile/', views.UpdateProfileView.as_view(), name='edit_profile'),
 
     # path('URL', views.VIEWS-FORM-CLASS-NAME.as_view(), name='PAGE-NAME'),
     ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .forms import UpdateProfileForm, RegistrationForm
 from django.urls import reverse_lazy
 
 # Create your views here.
-class NewUserRegistrationView(generic.CreateView):
-	form_class = UserCreationForm
+class UserRegistrationView(generic.CreateView):
+	form_class = RegistrationForm
 	template_name = "registration/register.html"
 	success_url = reverse_lazy('login')
+
+class UpdateProfileView(generic.UpdateView):
+	form_class = UpdateProfileForm
+	template_name = "registration/edit_profile.html"
+	success_url = reverse_lazy('home')
+
+	def get_object(self):
+		return self.request.user
