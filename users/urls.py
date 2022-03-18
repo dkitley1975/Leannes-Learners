ca8a8bcf213b58ff67529fd9b7e87914764d912b
David Kitley-McNamara <david@kitley-mcnamara.com>
2022-03-18 10:28:19 +0000

    fix: change background on the password-reset, and update url

--- users/urls.py
+++ users/urls.py
@@ -5,19 +5,26 @@ from users import views
 urlpatterns = [
     path('password/', views.PasswordUpdateView.as_view(),
          name='change-password'),
+    
     path('password_reset/', auth_views.PasswordResetView.as_view(),
          name='password_reset'),
+    
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
          name='password_reset_done'),
+    
     path('reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
+    
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
+    
     path('register/', views.UserRegistrationView.as_view(),
          name='register'),
+    
     path('edit-user-details', views.EditUserDetailsForm.as_view(),
          name='edit-user-details'),
+    
     path('edit-profile/<int:pk>', views.EditUserProfile.as_view(),
          name='edit-profile'),
 ]
