c8a466c1d502bfb5dd0c3878f19cc2b1d74606f0
David Kitley-McNamara <david@kitley-mcnamara.com>
2022-11-17 12:37:58 +0000

    update for render.com

--- leannes_learners/settings.py
+++ leannes_learners/settings.py
@@ -37,6 +37,11 @@ else:
     else:
         ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
 
+# Add Render.com URL to allowed hosts
+ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+ if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
